cf669f2a80a75cf0e5342675cae7259514902c4d
9tee <50616358+9tee@users.noreply.github.com>
2020-05-06 14:38:21 +0700

    chỉnh sửa layout để định hướng sang các trang phụ

--- BTL-ASP/Controllers/LoginController.cs
+++ BTL-ASP/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BTL_ASP.Models;
 
 namespace BTL_ASP.Controllers
 {
@@ -11,6 +12,11 @@ namespace BTL_ASP.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+        [HttpPost]
+        public string Login(User user)
+        {
+            return user.Id + " " + user.Password;
         }
     }
 }
